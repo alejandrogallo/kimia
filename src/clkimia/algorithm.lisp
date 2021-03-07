95c7017ee905a8536cc9b3874af97c9410b0c15c
Alejandro Gallo <aamsgallo@gmail.com>
2021-03-07 23:44:18 +0100

    Add some lisp helping functions

--- src/clkimia/algorithm.lisp
+++ src/clkimia/algorithm.lisp
@@ -1,7 +1,26 @@
 (defpackage :kimia/algorithm
-  (:nicknames :ka))
+  (:nicknames :ka)
+  (:export :mk-algorithm
+           :algorithm))
 
 (in-package :kimia/algorithm)
 
 (defmacro algorithm ()
   `(print "Algorithm"))
+
+(defun consume-in-out (lst &optional (tail '()))
+  (let ((first (car lst))
+        (rest (cdr lst)))
+    (cond
+      ((eq first :out) `(,(reverse tail) ,rest))
+      ((eq first :in) (consume-in-out rest tail))
+      ((eq lst '()) `(,(reverse tail) ,rest))
+      (t (consume-in-out rest (cons first tail))))))
+
+(defmacro mk-algorithm (name &rest args)
+  (let* ((in-out (consume-in-out args))
+         (in (car in-out))
+         (out (cadr in-out)))
+    `'(',name
+       :in ',in
+       :out ',out)))
--- src/clkimia/kimia.lisp
+++ src/clkimia/kimia.lisp
@@ -5,3 +5,9 @@
 
 (defun test ()
   (print "Hello From Kimia"))
+
+(defmacro wrap-input-script (arg)
+  `(progn
+     (print "LISP:: START evaluating KIMIA script")
+     ,arg
+     (print "LISP:: DONE evaluating KIMIA")))
